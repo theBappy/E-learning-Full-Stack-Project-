2ff971174eb6bda7a574d2b501ecaf764836c535
theBappy <abappy575@gmail.com>
2024-12-23 19:53:50 +0600

    user model created with mongoose model

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,19 @@
-const express
-= 
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Basic route
+app.get('/', (req,res)=>{
+    res.send('E-Learning Platform API')
+});
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+
